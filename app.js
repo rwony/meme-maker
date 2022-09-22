4710238c853b326babfb9854aecb97eb53cc4e5a
rwony <dev.rwlee@gmail.com>
2022-09-22 23:18:05 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.fill();
+ctx.rect(250, 250, 100, 100);
