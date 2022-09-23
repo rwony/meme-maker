da63ecfed7666cfafdb02335bf4282599d7dd2b1
rwony <dev.rwlee@gmail.com>
2022-09-23 22:58:29 +0900

    commit

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
+ctx.fillRect(210, 200, 15, 100);
+ctx.fillRect(360, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
+
+ctx.arc(290, 150, 50, 0, 2 * Math.PI);
 ctx.fill();
-ctx.rect(250, 250, 100, 100);
+
+ctx.beginPath();
+ctx.fillStyle = "#fff";
+ctx.arc(270, 150, 8, Math.PI, 2 * Math.PI);
+ctx.arc(310, 150, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
